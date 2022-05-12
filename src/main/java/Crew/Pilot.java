f11ae100a0880e483cd27c21ba48fae453356d16
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-05-12 23:19:59 +0100

    Created pilot class.

--- src/main/java/Crew/Pilot.java
+++ src/main/java/Crew/Pilot.java
@@ -1,4 +1,14 @@
 package Crew;
 
-public class Pilot {
+public class Pilot extends Crew{
+    private String licenseNumber;
+
+    public Pilot(String name, CrewRank rank, String licenseNumber) {
+        super(name, rank);
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
 }
--- src/test/java/CrewTest.java
+++ src/test/java/CrewTest.java
@@ -1,5 +1,6 @@
 import Crew.CabinCrewMember;
 import Crew.CrewRank;
+import Crew.Pilot;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,12 +8,15 @@ import static org.junit.Assert.assertEquals;
 
 public class CrewTest {
     private CabinCrewMember crew1, crew2;
+    private Pilot pilot1, pilot2;
 
 
     @Before
     public void before(){
         crew1 = new CabinCrewMember("Robert", CrewRank.CABINCREW);
         crew2 = new CabinCrewMember("Sarah", CrewRank.PURSER);
+        pilot1 = new Pilot("David", CrewRank.CAPTAIN, "JA12345");
+        pilot2 = new Pilot("Lucy", CrewRank.COPILOT, "JA55667");
     }
 
     @Test
@@ -24,5 +28,12 @@ public class CrewTest {
     public void doesCrewHaveRank(){
         assertEquals(CrewRank.CABINCREW, crew1.getRank());
         assertEquals(CrewRank.PURSER, crew2.getRank());
+        assertEquals(CrewRank.CAPTAIN, pilot1.getRank());
+        assertEquals(CrewRank.COPILOT, pilot2.getRank());
+    }
+
+    @Test
+    public void doesPilotHaveLicenseNumber(){
+        assertEquals("JA12345", pilot1.getLicenseNumber());
     }
 }
